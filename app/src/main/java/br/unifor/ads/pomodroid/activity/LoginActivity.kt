b635a444aab440ac4afe08f4896587cf2ff9d700
mauricio pereira <jmfcientista@gmail.com>
2018-05-20 18:38:12 -0300

    login usuario

--- app/src/main/java/br/unifor/ads/pomodroid/activity/LoginActivity.kt
+++ app/src/main/java/br/unifor/ads/pomodroid/activity/LoginActivity.kt
@@ -3,21 +3,38 @@ package br.unifor.ads.pomodroid.activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import br.unifor.ads.pomodroid.R
+import br.unifor.ads.pomodroid.dao.UserDAO
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var mEmailLogin: EditText
+    private lateinit var mSenhaLogin: EditText
+    private lateinit var mEntrarBtn: Button
     private lateinit var mLoginRegister: TextView
+    private lateinit var userDAO: UserDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        mEmailLogin = findViewById(R.id.email_login)
+        mSenhaLogin = findViewById(R.id.senha_login)
+
+        mEntrarBtn = findViewById(R.id.entrar_btn)
+        mEntrarBtn.setOnClickListener(this)
+
         mLoginRegister = findViewById(R.id.login_textview_register)
         mLoginRegister.setOnClickListener(this)
+
+        userDAO = UserDAO(this)
     }
 
     override fun onClick(view: View) {
@@ -28,6 +45,30 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
                 val it = Intent(this, RegisterFormActivity::class.java)
                 startActivity(it)
+
+            }
+
+            R.id.entrar_btn ->{
+
+                val users = userDAO.findAll()
+
+                Log.d("usuários:", users.toString())
+
+                for (user in users){
+                    if (user.email == mEmailLogin.text.toString()){
+                        if (user.password == mSenhaLogin.text.toString()){
+                            val it = Intent(this, MainActivity::class.java)
+                            startActivity(it)
+
+                            finish()
+                        }
+                    }
+
+
+                }
+
+                val toast = Toast.makeText(LoginActivity@this, "Dados do usuário incorretos.", Toast.LENGTH_SHORT)
+                toast.show()
 
             }
 
--- app/src/main/java/br/unifor/ads/pomodroid/activity/RegisterFormActivity.kt
+++ app/src/main/java/br/unifor/ads/pomodroid/activity/RegisterFormActivity.kt
@@ -1,13 +1,57 @@
 package br.unifor.ads.pomodroid.activity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import br.unifor.ads.pomodroid.R
+import br.unifor.ads.pomodroid.dao.UserDAO
+import br.unifor.ads.pomodroid.entity.User
 
-class RegisterFormActivity : AppCompatActivity() {
+class RegisterFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mNameEditText: EditText
+    private lateinit var mEmailEditText: EditText
+    private lateinit var mSenhaEditText: EditText
+    private lateinit var mSalvarBtn: Button
+    private lateinit var userDAO: UserDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_form)
+
+        mNameEditText = findViewById(R.id.edit_text_name)
+        mEmailEditText = findViewById(R.id.edit_text_email)
+        mSenhaEditText = findViewById(R.id.edit_text_senha)
+
+        mSalvarBtn = findViewById(R.id.salvar_btn)
+        mSalvarBtn.setOnClickListener(this)
+
+        userDAO = UserDAO(this)
+    }
+
+    override fun onClick(v: View) {
+
+        when(v.id){
+
+            R.id.salvar_btn -> {
+                val name = mNameEditText.text.toString()
+                val email = mEmailEditText.text.toString()
+                val password = mSenhaEditText.text.toString()
+
+                val user = User(null, name, email, password)
+                userDAO.insert(user)
+
+                val it = Intent(this, LoginActivity::class.java)
+                startActivity(it)
+
+                finish()
+            }
+
+        }
+
     }
 }

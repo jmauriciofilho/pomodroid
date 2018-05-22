261b9c00da624214203abfce7e05349f101b8715
mauricio pereira <jmfcientista@gmail.com>
2018-05-22 20:21:49 -0300

    delete lista de tarefas

--- app/src/main/java/br/unifor/ads/pomodroid/adapter/ListsTasksAdapter.kt
+++ app/src/main/java/br/unifor/ads/pomodroid/adapter/ListsTasksAdapter.kt
@@ -3,13 +3,15 @@ package br.unifor.ads.pomodroid.adapter
 import android.content.Context
 import android.content.Intent
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import android.widget.PopupMenu
 import android.widget.TextView
 import br.unifor.ads.pomodroid.R
+import br.unifor.ads.pomodroid.activity.MainActivity
 import br.unifor.ads.pomodroid.activity.TarefasActivity
 import br.unifor.ads.pomodroid.activity.TaskListFormEditActivity
+import br.unifor.ads.pomodroid.dao.TaskListDAO
 import br.unifor.ads.pomodroid.entity.TaskList
 
 class ListsTasksAdapter(val context: Context, val lists: List<TaskList>) : RecyclerView.Adapter<ListsTasksAdapter.TaskViewHolder>() {
@@ -19,12 +21,19 @@ class ListsTasksAdapter(val context: Context, val lists: List<TaskList>) : Recyc
         val name: TextView
         val description: TextView
         var id: Int
+        val option: TextView
+        val taskListDAO: TaskListDAO
 
         init {
 
             name = itemView.findViewById(R.id.item_list_task_name)
             description = itemView.findViewById(R.id.item_list_task_description)
+
+            option = itemView.findViewById(R.id.option_list_task)
+
             id = 0
+
+            taskListDAO = TaskListDAO(context)
 
             itemView.setOnClickListener(this)
             itemView.setOnLongClickListener(this)
@@ -64,6 +73,38 @@ class ListsTasksAdapter(val context: Context, val lists: List<TaskList>) : Recyc
         holder.name.text = lists[position].name
         holder.description.text = lists[position].description
         holder.id = lists[position].id!!
+
+        holder.option.setOnClickListener(View.OnClickListener {
+            //will show popup menu here
+            val popup = PopupMenu(context, holder.option)
+            //inflating menu from xml resource
+            popup.inflate(R.menu.menu_option_list_tasks)
+            //adding click listener
+            popup.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener {
+                override fun onMenuItemClick(item: MenuItem): Boolean {
+                    when (item.getItemId()) {
+                        R.id.editar_menu -> {
+
+                            val intent = Intent(context, TaskListFormEditActivity::class.java)
+                            intent.putExtra("idList", holder.id)
+                            context.startActivity(intent)
+
+                        }
+                        R.id.excluir_menu -> {
+
+                            val taskList = TaskList(holder.id, holder.name.text.toString(), holder.description.text.toString())
+                            holder.taskListDAO.delete(taskList)
+                            val it = Intent(context, MainActivity::class.java)
+                            context.startActivity(it)
+
+                        }
+                    }
+                    return false
+                }
+            })
+            //displaying the popup
+            popup.show()
+        })
     }
 
 }
